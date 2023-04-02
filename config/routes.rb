8dd99ee70422df8b0abca808d34ad2661c7d96d9
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-02 10:37:02 +0000

    [Finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  resources:lists
 end
